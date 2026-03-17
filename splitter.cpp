ed92b65b0e5c9b204a69b84a6fbbc654bfa4fd7c
detolly <thomas@detolly.no>
2026-03-17 15:23:39 +0100

    pb+sob comparison

--- splitter.cpp
+++ splitter.cpp
@@ -234,7 +234,6 @@ void database::load_from_disk()
     std::println(stderr, "Loaded {} runs from {}", runs.size(), FILENAME);
 }
 
-std::optional<run> comparing_to;
 running_run current_run;
 
 constexpr const char* BOLD_YELLOW = "\033[1;33m";
@@ -249,31 +248,41 @@ void print_table()
 {
     const auto& r = current_run.current_run;
     uint32_t total = 0;
-    uint32_t cmp_total = 0;
+    uint32_t pb_frames_total = 0;
+    uint32_t sob_frames_total = 0;
+
+    const auto pb = database::instance.pb;
+    const auto sob = database::instance.best_segments;
+
+    bool pb_scuffed = false;
+    bool sob_scuffed = false;
 
     for (auto i = 0u; i < NUM_SEGMENTS; ++i) {
-        std::print("{:>2} | ", i + 1);
 
+        // Current segment
+        std::print("| {:>2} | ", i + 1);
+
+        auto segment_frames = r.segments[i].frames;
         const bool is_current_segment = i == current_run.current_segment;
         const bool is_finished_segment = i < current_run.current_segment;
-        const bool is_gold = is_finished_segment && (r.segments[i].frames < database::instance.get_best_segments().segments[i].frames || database::instance.get_best_segments().segments[i].frames < 0);
+        const bool is_gold = is_finished_segment && (segment_frames < database::instance.get_best_segments().segments[i].frames || database::instance.get_best_segments().segments[i].frames < 0);
 
-        total += r.segments[i].frames;
+        if (segment_frames > -1)
+            total += segment_frames;
+
+        if (is_current_segment)
+            total += current_run.current_frame;
+
+        segment_frames = (is_current_segment ? (ssize_t)current_run.current_frame : segment_frames);
 
         // Total
-        if (!is_finished_segment)
-            std::print("{:17} | ", "");
-        else
+        if (segment_frames > -1)
             std::print("{:<8.2f} {:>8} | ", (double)total / 30.0, total);
-
-        uint32_t segment_frames = 0;
-        if (is_finished_segment)
-            segment_frames = static_cast<uint32_t>(r.segments[i].frames);
-        else if (is_current_segment)
-            segment_frames = current_run.current_frame;
+        else
+            std::print("{:17} | ", "");
 
         // Segment
-        if (segment_frames > 0 || is_current_segment)
+        if (segment_frames > -1)
             std::print("{}{:<8.2f} {:>8}{} | ",
                        is_gold ? BOLD_YELLOW : RESET,
                        static_cast<double>(segment_frames) / 30.0,
@@ -282,26 +291,16 @@ void print_table()
         else
             std::print("{:17} | ", "");
 
-        // Comparison
-        if (!comparing_to.has_value()) {
-            std::println();
-            continue;
-        }
+        // PB compare
+        if (!pb_scuffed && pb.has_value() && pb.value().segments[i].frames > -1 && (is_finished_segment || is_current_segment)) {
+            const auto& pb_run = pb.value();
 
-        const auto& cmp = comparing_to.value();
-        const auto cmp_frames = cmp.segments[i].frames;
-        if (cmp_frames < 0) {
-            std::println();
-            continue;
-        }
+            const auto pb_segment_frames = pb_run.segments[i].frames;
+            pb_frames_total += pb_run.segments[i].frames;
 
-        cmp_total += cmp.segments[i].frames;
-
-        if (is_current_segment || is_finished_segment)
-        {
-            const auto total_diff = static_cast<int32_t>(total + (is_current_segment ? current_run.current_frame : 0)) - static_cast<int32_t>(cmp_total);
+            const auto total_diff = static_cast<int32_t>(total) - static_cast<int32_t>(pb_frames_total);
             const auto total_diff_sec = (double)total_diff / 30.0;
-            const auto diff_frames = static_cast<int32_t>(segment_frames) - static_cast<int32_t>(cmp_frames);
+            const auto diff_frames = static_cast<int32_t>(segment_frames) - static_cast<int32_t>(pb_segment_frames);
             const double diff_sec = static_cast<double>(diff_frames) / 30.0;
 
             const char* segment_color = diff_frames < 0 ? GREEN : 
@@ -315,10 +314,40 @@ void print_table()
             if (is_gold)
                 segment_color = BOLD_YELLOW;
 
-            std::print("{}{:<+8.2f} {}{:^+8.2f} {:>+8}{}", total_color, total_diff_sec, segment_color, diff_sec, diff_frames, RESET);
+            std::print("{}{:<+8.2f} {}{:^+8.2f} {:>+8}{} | ", total_color, total_diff_sec, segment_color, diff_sec, diff_frames, RESET);
+        } else {
+            std::print("{:26} | ", " ");
+            pb_scuffed = true;
         }
 
-        std::println();
+        // golds compare
+        if (!sob_scuffed && sob.segments[i].frames > -1 && (is_finished_segment || is_current_segment)) {
+            const auto sob_segment_frames = sob.segments[i].frames;
+            sob_frames_total += sob.segments[i].frames;
+
+            const auto total_diff = static_cast<int32_t>(total) - static_cast<int32_t>(sob_frames_total);
+            const auto total_diff_sec = (double)total_diff / 30.0;
+            const auto diff_frames = static_cast<int32_t>(segment_frames) - static_cast<int32_t>(sob_segment_frames);
+            const double diff_sec = static_cast<double>(diff_frames) / 30.0;
+
+            const char* segment_color = diff_frames < 0 ? GREEN : 
+                                        diff_frames > 0 ? RED :
+                                        RESET;
+
+            const char* total_color = total_diff < 0 ? GREEN :
+                                      total_diff > 0 ? RED :
+                                      RESET;
+
+            if (is_gold)
+                segment_color = BOLD_YELLOW;
+
+            std::print("{}{:<+8.2f} {}{:^+8.2f} {:>+8}{} | ", total_color, total_diff_sec, segment_color, diff_sec, diff_frames, RESET);
+        } else {
+            std::print("{:26} | ", " ");
+            sob_scuffed = true;
+        }
+
+        std::print("\n");
     }
 }
 
@@ -326,15 +355,20 @@ void print_run()
 {
     std::println("\033[2J\033[H");  // clear screen + home
 
-    std::println(" {}#{} | {}{:^17}{} | {}{:^17}{} | {}{:^17}{} ", 
-                 CYAN, RESET,
+    constexpr static const char* line = "--------------------------------------------------------------------------------------------------------";
+
+    std::println(line);
+
+    std::println("| {}{:>2}{} | {}{:^17}{} | {}{:^17}{} | {}{:^26}{} | {}{:^26}{} |", 
+                 CYAN, "#", RESET,
                  CYAN, "Total", RESET,
                  CYAN, "Segment", RESET,
-                 CYAN, "Comparison", RESET);
+                 CYAN, "PB", RESET,
+                 BOLD_YELLOW, "SOB", RESET);
 
     print_table();
 
-    std::println("------------------------------------------------------------------------------");
+    std::println(line);
 
     uint32_t total_frames = current_run.current_frame;
     for (uint32_t i = 0; i < current_run.current_segment; ++i)
@@ -349,17 +383,10 @@ void print_run()
     std::println("sob: {:.2f} {:4}", sob_time, sob_frames);
 }
 
-void update_comparing_to()
-{
-    comparing_to = database::instance.get_pb();
-}
-
 void reset()
 {
     if (current_run.current_run.segments[0].frames > -1)
         database::instance.save_run(current_run.current_run);
-
-    update_comparing_to();
 
     current_run = running_run{};
     current_run.current_run.started_at = (uint64_t)std::time(NULL);
@@ -367,15 +394,16 @@ void reset()
 
 void split()
 {
-    if (current_run.finished)
+    if (current_run.finished) {
+        std::println(stderr, "warning: tried to split when finished.");
         return;
+    }
 
     current_run.split();
     print_run();
     if (current_run.finished) {
         current_run.current_run.did_finish = true;
         database::instance.save_run(current_run.current_run);
-        update_comparing_to();
     }
 }
 
@@ -415,33 +443,32 @@ void socket_thread()
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(12345);
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
-        close(server_fd);
+    while (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
         std::println(stderr, "can't bind");
-        exit(1);
+        sleep(2);
     }
 
     if (listen(server_fd, 1) == -1) {
         close(server_fd);
         std::println(stderr, "can't listen");
-        exit(1);
+        return;
     }
 
     std::println(stderr, "listening");
 
-    while (1) {
-        client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
-        if (client_fd == -1)
-            continue;
+    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+    if (client_fd == -1) {
+        std::println(stderr, "client broken");
+        return;
+    }
 
-        std::println(stderr, "accepted client");
+    std::println(stderr, "accepted client");
 
-        while(1) {
-            ssize_t n = read(client_fd, buffer, sizeof(buffer) - 1);
-            if (n > 0) {
-                process_received(buffer, static_cast<uint32_t>(n));
-            } else break;
-        }
+    while(1) {
+        ssize_t n = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (n > 0) {
+            process_received(buffer, static_cast<uint32_t>(n));
+        } else break;
     }
 
     close(server_fd);
@@ -450,7 +477,5 @@ void socket_thread()
 int main()
 {
     database::instance.load_from_disk();
-    comparing_to = database::instance.get_pb();
-
     socket_thread();
 }
